41367405d7a4caa28622b8a6f38b14ab059523ff
waleedshah0 <110971404+waleedshah0@users.noreply.github.com>
2024-02-06 13:47:46 +0500

    Add files via upload

---
+++ fetch1.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const signupForm = document.getElementById('form2');
+  
+    signupForm.addEventListener('submit', async (event) => { // Change 'button' to 'submit'
+      event.preventDefault();
+  
+      const formData = new FormData(signupForm);
+      const data = {};
+  
+      formData.forEach((value, key) => {
+        data[key] = value;
+      });
+  
+      const response = await fetch('http://localhost:3000/fetch1', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+});
+
+const result = await response.json();
+console.log(result);
+
+if (result.nextStep) {
+  window.location.href = result.nextStep;
+}
+
+    });
+});
+
+  
+
+
+
+
+
+
+
